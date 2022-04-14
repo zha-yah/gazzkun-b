9a0db4f283a6212d8f02b0debb7ed36686c31002
zhayah <zhafari2014@gmail.com>
2022-04-14 11:34:52 +0700

    Update categories

--- src/components/Homepage/Categories/Category.js
+++ src/components/Homepage/Categories/Category.js
@@ -6,29 +6,29 @@ const Category = () => {
     <>
       <div className='categories'>Categories</div>
       <div className='cat-con-1'/>
-      <div className='cat-txt-1'>Anime</div>
+      <div className='cat-txt-1'>Fashion</div>
       <div className='cat-con-2'/>
-      <div className='cat-txt-2'>Anime</div>
+      <div className='cat-txt-2'>Cosplay</div>
       <div className='cat-con-3'/>
-      <div className='cat-txt-3'>Anime</div>
+      <div className='cat-txt-3'>Accesories</div>
       <div className='cat-con-4'/>
-      <div className='cat-txt-4'>Anime</div>
+      <div className='cat-txt-4'>Figma</div>
       <div className='cat-con-5'/>
-      <div className='cat-txt-5'>Anime</div>
+      <div className='cat-txt-5'>Nendoroid</div>
       <div className='cat-con-6'/>
-      <div className='cat-txt-6'>Anime</div>
+      <div className='cat-txt-6'>PVC Figure</div>
       <div className='cat-con-7'/>
-      <div className='cat-txt-7'>Anime</div>
+      <div className='cat-txt-7'>Model Kit</div>
       <div className='cat-con-8'/>
-      <div className='cat-txt-8'>Anime</div>
+      <div className='cat-txt-8'>Plushies</div>
       <div className='cat-con-9'/>
-      <div className='cat-txt-9'>Anime</div>
+      <div className='cat-txt-9'>EN Light Novel</div>
       <div className='cat-con-10'/>
-      <div className='cat-txt-10'>Anime</div>
+      <div className='cat-txt-10'>EN Manga</div>
       <div className='cat-con-11'/>
-      <div className='cat-txt-11'>Anime</div>
+      <div className='cat-txt-11'>JP Light Novel</div>
       <div className='cat-con-12'/>
-      <div className='cat-txt-12'>Anime</div>
+      <div className='cat-txt-12'>JP Manga</div>
     </>
   )
 }
